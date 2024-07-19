a22115936fe6a43b0c5e344690a8c4b893968dd6
David Cote <dmc1991@icloud.com>
2024-07-19 09:30:27 -0400

    Add Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,11 @@
+import Header from "./components/Header";
+
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default App;
