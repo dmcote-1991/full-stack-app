c09310be58ef0800dcec85faa5d8d3842f3419c2
David Cote <dmc1991@icloud.com>
2024-07-22 23:13:56 -0400

    Persists User Credentials

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -3,7 +3,10 @@ import { createContext, useState, useContext, useEffect } from "react";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
 
   const signIn = async (credentials) => {
     try {
@@ -16,8 +19,10 @@ export const AuthProvider = ({ children }) => {
           Authorization: `Basic ${authToken}`,
         },
       });
+
       if (response.ok) {
         const data = await response.json();
+        localStorage.setItem("user", JSON.stringify({ ...data, emailAddress }));
         setUser({ ...data, emailAddress, password });
         return data;
       } else {
@@ -30,7 +35,12 @@ export const AuthProvider = ({ children }) => {
   };
 
   const signOut = async () => {
-    setUser(null);
+    try {
+      localStorage.removeItem("user");
+      setUser(null);
+    } catch (error) {
+      console.error("Sign-out error:", error);
+    }
   };
 
   useEffect(() => {
